1123d8b72d49db32a2258c19b74bb65e287a0d82
dgwowxyz <cyanverya@gmail.com>
2026-05-18 11:13:53 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -105,14 +105,14 @@
 
 	local themes = {
 		preset = {
-			["outline"] = hex("#0A0A0A"), -- 
-			["inline"] = hex("#2D2D2D"), --
-			["accent"] = hex("#6078BE"), --
-			["high_contrast"] = hex("#141414"),
-			["low_contrast"] = hex("#1E1E1E"),
-			["text"] = hex("#B4B4B4"),
-			["text_outline"] = rgb(0, 0, 0),
-			["glow"] = hex("#6078BE"), 
+		    ["outline"] = hex("#08080A"),
+		    ["inline"] = hex("#2A2A32"),
+		    ["accent"] = hex("#A855F7"),
+		    ["high_contrast"] = hex("#101014"),
+		    ["low_contrast"] = hex("#16161E"),
+		    ["text"] = hex("#FFFFFF"),
+		    ["text_outline"] = rgb(0, 0, 0), 
+		    ["glow"] = hex("#A855F7"),
 		},
 
 		utility = {
